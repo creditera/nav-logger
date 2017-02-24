77f3bea926fe97d7cb062788b10b7ab7653fe4dd
Nathan Davis <nbriar@users.noreply.github.com>
2017-02-24 16:29:25 -0700

    Update request_tagger.rb
    Missing a namespace.

--- lib/nav/logger/middleware/request_tagger.rb
+++ lib/nav/logger/middleware/request_tagger.rb
@@ -1,34 +1,36 @@
 module Nav
   module Logger
-    class RequestTagger
+    module Middleware
+      class RequestTagger
 
-      def initialize(app)
-        @app = app
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          setup_request_store
+          store_request_var env
+          @app.call env
+        ensure
+          clear_request_store
+        end
+
+        private
+          def setup_request_store
+            RequestStore.begin!
+          end
+
+          def store_request_var(env)
+            RequestStore[:request_id] = env["HTTP_X_REQUEST_ID"] || SecureRandom.uuid
+            RequestStore[:session_id] = env["HTTP_X_SESSION_ID"] if env.key? "HTTP_X_SESSION_ID"
+          end
+
+          def clear_request_store
+            RequestStore.end!
+            RequestStore.clear!
+          end
+
       end
-
-      def call(env)
-        setup_request_store
-        store_request_var env
-        @app.call env
-      ensure
-        clear_request_store
-      end
-
-      private
-        def setup_request_store
-          RequestStore.begin!
-        end
-
-        def store_request_var(env)
-          RequestStore[:request_id] = env["HTTP_X_REQUEST_ID"] || SecureRandom.uuid
-          RequestStore[:session_id] = env["HTTP_X_SESSION_ID"] if env.key? "HTTP_X_SESSION_ID"
-        end
-
-        def clear_request_store
-          RequestStore.end!
-          RequestStore.clear!
-        end
-
     end
   end
 end
